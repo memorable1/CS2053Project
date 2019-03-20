3049384a998a21a3fe9ebddcedf8f4fcd701c226
Memorab1e <jwiebe1@unb.ca>
2019-03-20 20:29:45 -0300

    first march 20

---
+++ Take 2/Assets/Scripts/KeyTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyTrigger : MonoBehaviour {
+
+    public GameObject UnopenedBox;
+    public GameObject key;
+    public GameObject MUSIC;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            //MUSIC.SetActive(true);
+            UnopenedBox.SetActive(false);
+            key.transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+}
+
---
+++ Take 2/Assets/Scripts/SpikeTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            Destroy(c.gameObject);           
+        }
+    }
+}
